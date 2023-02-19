e917fbce6f22384094a58fc996acb045c8bbe14c
Rowyda020 <rowydaramadan@gmail.com>
2023-02-19 09:44:55 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main -A program that prints a line with puts function
+ * return: 0 (success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
