c45f7d591cee0894bf661d1d299bb70298d1656d
Rowyda020 <rowydaramadan@gmail.com>
2023-02-22 16:15:19 -0800

    0x02. c - functions nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabet in lowecase 
+ */
+void print_alphabet(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
