8308e5fa8416a112f8a175db2133a84b5f05d4c5
Rowyda020 <rowydaramadan@gmail.com>
2023-02-24 16:11:26 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
- * main - causes an infinite loop
+ * main - Test function for positive or negative
  * Return: 0
  */
 
@@ -9,16 +9,8 @@ int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming :(\n");
-
-	/**
-	 * i = 0;
-	 *while (i < 10)
-	 *{
-	 *	putchar(i);
-	 *}
-	 */
-	printf("Infinite loop avoided! \\o/\n");
+	i = 0;
+	positive_or_negative(i);
 
 	return (0);
 }
